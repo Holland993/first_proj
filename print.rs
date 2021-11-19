45a669fd2ae62e735857a6824b980720bbce1a99
Holland993 <pathf274@gmail.com>
2021-11-19 19:03:33 +0200

    okay

--- print.rs
+++ print.rs
@@ -1,4 +1,12 @@
 pub fn run() {
     //print to console
     println!("Hello from the other side");
+
+    //basic formatting
+    println!("Number {}.", 1);
+
+    println!("{} is from {}", "Brad", "mass");
+
+    // Postional Arguments
+    println!("{0} is the {1} and the {2}. It was always {0}!!!", "Jessi", "Rat", "King");
 }
